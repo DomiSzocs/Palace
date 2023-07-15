8bb0a682150e2df3b9defb39e9cb78500dfc0777
DomiSzocs <domiszocs@gmail.com>
2023-07-15 10:46:42 +0300

    Remove unnecesary files

--- client/src/pages/api/config.js
+++
@@ -1,9 +0,0 @@
-import fs from 'fs'
-import path from 'path';
-
-export default async function handler(req, res) {
-    const configPath = path.join('src', 'util', 'cardLayout.json')
-    await fs.readFile(configPath, 'utf-8', (err, data) => {
-        res.status(200).send({config:data});
-    });
-}
--- client/src/util/dealer.js
+++ client/src/util/dealer.js
@@ -1,36 +1,36 @@
-import Deck from "./deck"
-
-export default class Dealer {
-    constructor(numberOfPlayers, numberOfDecks) {
-        this.numberOfPlayers = numberOfPlayers;
-        this.deck = new Deck(numberOfDecks);
-    }
-
-    dealToPlayer = (deck, playerName) => {
-        return {
-            faceDown: dealThree(deck),
-            faceUp: dealThree(deck),
-            hand: dealThree(deck),
-            uid: playerName
-        }
-    }
-
-    deal = () => {
-        this.deck.shuffle();
-        let players = [];
-        for (let i = 0; i < this.numberOfPlayers; i++) {
-            players.push(this.dealToPlayer(this.deck, i));
-        }
-        return players
-    };
-}
-
-const dealThree = (deck) => {
-    const pile = [];
-    for (let i = 0; i < 3; i++) {
-        const topCard = deck.getTopCard();
-        pile.push({rank: topCard.rank, suit: topCard.suit})
-        deck.cards.pop();
-    }
-    return pile;
-}
+// import Deck from "./deck"
+//
+// export default class Dealer {
+//     constructor(numberOfPlayers, numberOfDecks) {
+//         this.numberOfPlayers = numberOfPlayers;
+//         this.deck = new Deck(numberOfDecks);
+//     }
+//
+//     dealToPlayer = (deck, playerName) => {
+//         return {
+//             faceDown: dealThree(deck),
+//             faceUp: dealThree(deck),
+//             hand: dealThree(deck),
+//             uid: playerName
+//         }
+//     }
+//
+//     deal = () => {
+//         this.deck.shuffle();
+//         let players = [];
+//         for (let i = 0; i < this.numberOfPlayers; i++) {
+//             players.push(this.dealToPlayer(this.deck, i));
+//         }
+//         return players
+//     };
+// }
+//
+// const dealThree = (deck) => {
+//     const pile = [];
+//     for (let i = 0; i < 3; i++) {
+//         const topCard = deck.getTopCard();
+//         pile.push({rank: topCard.rank, suit: topCard.suit})
+//         deck.cards.pop();
+//     }
+//     return pile;
+// }
